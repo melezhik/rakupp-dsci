ed378b2d4bd67e71b32c58656295f53c981dd16a
Alexey Melezhik <melezhik@gmail.com>
2026-07-31 12:20:15 +0300

    Delete .dsci/shared/tasks/package_windows_mingw directory

--- .dsci/shared/tasks/package_windows_mingw/task.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-NAME=$(config NAME)
-echo "Shared: package windows mingw $NAME"
-zip -q -r "rakupp-${NAME}.zip" dist/rakupp || true
-sha256sum "rakupp-${NAME}.zip" > "rakupp-${NAME}.zip.sha256" || true
