41ad4627ae7edca1ddb91ee3721be4d17365fa26
Alexey Melezhik <melezhik@gmail.com>
2026-07-31 12:21:30 +0300

    Delete .dsci/shared/tasks/smoke_exe_windows_mingw directory

--- .dsci/shared/tasks/smoke_exe_windows_mingw/task.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-echo "Shared: smoke_exe_windows_mingw"
-build/rakupp.exe --exe tools/smoke.raku -o smoke-native.exe
-./smoke-native.exe
-code=$?
-echo "native exit code: $code"
-exit $code
